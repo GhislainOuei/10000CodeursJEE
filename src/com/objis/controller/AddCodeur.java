9f71467712c94faee3db2e6069c9f50b982d510d
OUEI Ghislain <ghislainouei@gmail.com>
2018-06-05 10:21:08 +0000

    attente de correction des erreurs sur pattern

--- src/com/objis/controller/AddCodeur.java
+++ src/com/objis/controller/AddCodeur.java
@@ -1,6 +1,6 @@
 package com.objis.controller;
 
-import java.io.IOException;
+import java.io.IOException; 
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,8 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+ 
 
  
 @WebServlet(description = "permet de gerer l'inscription a 10000 Codeur", urlPatterns = { "/inscription" })
@@ -30,11 +29,17 @@ public class AddCodeur extends HttpServlet {
  
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 request.setAttribute("erreur", false);
-		 //definitiion d'une expression regulier (model)
-		 String patternDate = "^\\d{1,2}\\/\\d{1,2}\\/\\{4}$";
-		 Pattern p = Pattern.com(patternDate);
+		
+		 
 		 String nom = request.getParameter("nom");
 		 System.out.println("nom: "+nom);
+		 
+		 //definitiion d'une expression regulier (model)
+//		 String pattern  = "^\\d{1,2}\\/\\d{1,2}\\/\\{4}$";
+		 
+//		 Pattern r = Pattern.compile(pattern);
+//		 Matcher m = r.matcher();
+//		 
 		 if(nom.length() == 0) {
 			 request.setAttribute("erreur", true);
 			 request.setAttribute("erreur_nom", true);
@@ -48,18 +53,19 @@ public class AddCodeur extends HttpServlet {
 			 System.out.println("erreur sur le prenom");
 		 }
 		 
-//		 if(nom.length() == 0) {
-//			 request.setAttribute("erreur", true);
-//			 request.setAttribute("erreur_nom", true);
-//			 System.out.println("erreur sur le nom");
-//		 }
-//		 
-//		 if(nom.length() == 0) {
-//			 request.setAttribute("erreur", true);
-//			 request.setAttribute("erreur_nom", true);
-//			 System.out.println("erreur sur le nom");
-//		 }
-//		 
+		String email = request.getParameter("email");
+		 if(email.length() == 0) {
+			 request.setAttribute("erreur", true);
+			 request.setAttribute("erreur_nom", true);
+			 System.out.println("erreur sur le nom");
+		 }
+		 
+		 if(nom.length() == 0) {
+			 request.setAttribute("erreur", true);
+			 request.setAttribute("erreur_nom", true);
+			 System.out.println("erreur sur le nom");
+		 }
+		 
 		 System.out.println("***************fin requete*************");
 		doGet(request, response);
 	}
